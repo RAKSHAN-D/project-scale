55d5863a4d6fe669c064cdcdf794c3cca4f259a5
Rakshan R Doddamani <rakshandn@gmail.com>
2026-04-19 14:17:21 +0530

    fix : update ColdItemController to use @Valid and remove userId from delete endpoint

--- Backend/src/main/java/com/projectscale/projectscale/controller/ColdItemController.java
+++ Backend/src/main/java/com/projectscale/projectscale/controller/ColdItemController.java
@@ -1,5 +1,6 @@
 package com.projectscale.projectscale.controller;
 import java.util.List;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity; 
 import org.springframework.web.bind.annotation.* ;
 import com.projectscale.projectscale.dto.ColdDataRequest;
@@ -13,24 +14,28 @@ public class ColdItemController {
         this.coldDataService = coldDataService; // Constructor injection of the service
     }
     @PostMapping
-    public ResponseEntity<ColdDataResponse> addColdData(@RequestBody ColdDataRequest request) {
+    public ResponseEntity<ColdDataResponse> addColdData(@Valid @RequestBody ColdDataRequest request) {
         return ResponseEntity.status(201).body(coldDataService.addColdData(request));
     }
     @GetMapping("/user/{userId}")
     public ResponseEntity<List<ColdDataResponse>> getColdDataForUser(@PathVariable Long userId) {
         return ResponseEntity.ok(coldDataService.getColdDataForUser(userId));
     }
+    @GetMapping("item/{itemId}")
+    public ResponseEntity<ColdDataResponse> getColdDataForUserAndItem(@PathVariable Long itemId) {
+        return ResponseEntity.ok(coldDataService.getColdDataForUserAndItem(itemId));
+    }
     @GetMapping
     public ResponseEntity<List<ColdDataResponse>> getAllColdData() {
         return ResponseEntity.ok(coldDataService.getAllColdData());
     }
     @PutMapping("/{id}")
-    public ResponseEntity<ColdDataResponse> updateColdData(@PathVariable Long id, @RequestBody ColdDataRequest request) {
+    public ResponseEntity<ColdDataResponse> updateColdData(@PathVariable Long id, @Valid @RequestBody ColdDataRequest request) {
         return ResponseEntity.ok(coldDataService.updateColdData(id, request));
     }
-    @DeleteMapping("/user/{userId}")
-    public ResponseEntity<Void> deleteColdDataForUser(@PathVariable Long userId) {
-        coldDataService.deleteColdDataForUser(userId);
+    @DeleteMapping("item/{itemId}")
+    public ResponseEntity<Void> deleteColdData( @PathVariable Long itemId) {
+        coldDataService.deleteColdData(itemId);
         return ResponseEntity.noContent().build();
     }
 }
