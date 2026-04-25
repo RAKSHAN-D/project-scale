8981971c84438ee6fa926dc76fe91b7ffda116ad
Rakshan R Doddamani <rakshandn@gmail.com>
2026-04-25 23:54:13 +0530

    fix : SeedAdmin.java, Check if admin already exists before seeding using existsByEmail method and also added password encoding using passwordEncoder bean.

--- Backend/src/main/java/com/projectscale/projectscale/config/SeedAdmin.java
+++ Backend/src/main/java/com/projectscale/projectscale/config/SeedAdmin.java
@@ -2,7 +2,7 @@ package com.projectscale.projectscale.config;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import com.projectscale.projectscale.entity.User;
 import com.projectscale.projectscale.repository.UserRepository;
 @Configuration
@@ -10,14 +10,18 @@ public class SeedAdmin {
     @Bean
     CommandLineRunner init(UserRepository userRepository) {
         return args -> {
-             
+               String password = "admin123";
+               BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+               String hashedPassword = passwordEncoder.encode(password);
+               if (!userRepository.existsByemail("admin@gmail.com")) {
                 User admin = new User();
                 admin.setUsername("admin");
                 admin.setEmail("admin@gmail.com");
-                admin.setPasswordHash("$2a$10$7Qy8s1m"); // Pre-hashed password for "admin123"
+                admin.setPasswordHash(hashedPassword); // Pre-hashed password for "admin123"
                 admin.setRole(User.Role.ADMIN);
                 userRepository.save(admin);
                 System.out.println("Admin user created: admin / admin123");
             }
         };
+}
 }
