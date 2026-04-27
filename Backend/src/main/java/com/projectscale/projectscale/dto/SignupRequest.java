26636eb076d07c2d833bc604d5ad687f42695365
Rakshan R Doddamani <rakshandn@gmail.com>
2026-04-27 11:51:12 +0530

    feat: Added signup request DTO for user registration endpoint.

---
+++ Backend/src/main/java/com/projectscale/projectscale/dto/SignupRequest.java
@@ -0,0 +1,38 @@
+package com.projectscale.projectscale.dto;
+import jakarta.validation.constraints.*;
+public class SignupRequest {
+    @NotBlank(message = "Username is required")
+    private String username;
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long.")
+    private String password;
+
+    public SignupRequest() {}
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
